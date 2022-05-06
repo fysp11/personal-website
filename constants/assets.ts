b69fae01f592e485d0c4a5c692fbf6ebfd2d6a93
Felipe Mendonca <fysp11@gmail.com>
2022-05-06 13:25:29 +0200

    fixed irl avatar size

--- constants/assets.ts
+++ constants/assets.ts
@@ -1,4 +1,4 @@
 const AVATAR_PNG = '/static/images/avatar.png';
-const IRL_AVATAR_JPEG = '/static/images/irl_avatar.jpeg';
+const IRL_AVATAR_JPEG = '/static/images/irl_avatar.jpg';
 
 export { AVATAR_PNG, IRL_AVATAR_JPEG };
