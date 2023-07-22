06be22f25758525d1d82b7c08f63babb1625fc20
fysp11 <fysp11@gmail.com>
2023-07-22 21:55:18 +0200

    chore(next.config.js): add support for bundle analyzer plugin to analyze the size of the bundles chore(next.config.js): add security headers to improve security of the application chore(next.config.js): enable SWC minification for improved performance chore(next.config.js): enable React strict mode for better development experience chore(next.config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,64 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true"
+})
+
+const ContentSecurityPolicy = `
+  default-src * data:;
+  style-src 'self' 'unsafe-inline';
+  style-src-elem 'self' https://fonts.googleapis.com/ 'unsafe-inline';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://vitals.vercel-insights.com/ https://www.googletagmanager.com/ https://www.google-analytics.com/;
+  font-src 'self' https://fonts.gstatic.com/ 'unsafe-inline';
+  object-src 'none';
+`
+
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on"
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload"
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block"
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN"
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()"
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff"
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin"
+  },
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim()
+  }
+]
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: true,
+  reactStrictMode: true,
   // output: "standalone",
-  output: "export",
-  distDir: "dist",
-  images: {
-    unoptimized: true,
-    domains: ["localhost"],
-    remotePatterns: [
+  // output: "export",
+  // distDir: "dist",
+  images: {},
+  async headers() {
+    return [
       {
-        protocol: "https",
-        hostname: "picsum.photos",
-        port: "",
-        pathname: "/**"
+        source: "/:path*",
+        headers: securityHeaders
       }
     ]
   }
