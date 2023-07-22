00e1209101ae2efd4f339d4fc3ccf6da8745c6de
fysp11 <fysp11@gmail.com>
2023-07-22 21:36:24 +0200

    chore(layout.tsx): add Google Analytics script to track page views and user interactions feat(layout.tsx): add support for loading Google Analytics script dynamically using next/script for better performance and lazy loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import NavBar from "@/components/layouts/nav/navbar"
 import "@/styles/globals.css"
 
 import type { PropsWithChildren } from "react"
+import Script from "next/script"
 
 import Container from "@/components/ui/Container"
 import Footer from "@/components/layouts/footer"
@@ -34,11 +35,19 @@ export default function RootLayout({ children }: PropsWithChildren) {
             <Footer />
           </div>
         </Providers>
-        <GoogleAnalytics
-          strategy="lazyOnload"
-          debugMode={true}
-          trackPageViews
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
         />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+        `}
+        </Script>
+        <GoogleAnalytics debugMode={true} trackPageViews />
       </body>
     </html>
   )
