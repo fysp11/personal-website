f8acfb00743fad47d31a5e256f0b56d7a14426e9
Felipe Mendonça <fysp11@gmail.com>
2023-12-22 16:20:45 +0100

    Add VS Code extensions and update layout components (#85)
    * Add VS Code extensions and update layout components
    
    * Remove AI PR Reviewer workflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,26 +3,26 @@ import NavBar from "@/components/layouts/nav/navbar"
 import "@/styles/globals.css"
 
 import type { PropsWithChildren } from "react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { cn } from "@/lib/utils"
 import Container from "@/components/ui/Container"
 import Footer from "@/components/layouts/footer"
-import Providers from "@/components/providers"
+import Analytics from "@/components/wrappers/analytics"
+import Providers from "@/components/wrappers/providers"
 
 export default function RootLayout({ children }: PropsWithChildren) {
   const styles = {
     container: cn("px-[14px]", "w-full sm:max-w-xl md:max-w-2xl")
   }
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body>
         <Providers>
           <NavBar />
           <Container className={styles.container}>{children}</Container>
           <Footer />
         </Providers>
-        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   )
