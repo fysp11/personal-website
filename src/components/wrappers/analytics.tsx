bdbc5209bd4631e14f3b35ab4ecb4c8a20333cae
fysp11 <fysp11@gmail.com>
2024-05-20 12:23:01 +0200

    feat: Update Google Analytics implementation

--- src/components/wrappers/analytics.tsx
+++ src/components/wrappers/analytics.tsx
@@ -6,22 +6,36 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 import { GoogleAnalytics } from "nextjs-google-analytics"
 
 export default function Analytics() {
+  const measurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
+
+  if (!measurementId) {
+    console.error("Google Analytics measurement ID is not defined")
+    return null
+  }
+
   return (
     <>
       <SpeedInsights />
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+        strategy="afterInteractive"
       />
-      <Script id="google-analytics">
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+          gtag('config', '${measurementId}');
         `}
       </Script>
-      <GoogleAnalytics trackPageViews defaultConsent="denied" />
+      <GoogleAnalytics
+        debugMode={process.env.NODE_ENV === "development"}
+        strategy="afterInteractive"
+        gaMeasurementId={measurementId}
+        trackPageViews
+        defaultConsent="denied"
+      />
     </>
   )
 }
