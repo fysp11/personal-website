a9573373e7d5391c58a6a6a641dae8016476bf9b
Felipe Mendonça <fysp11@gmail.com>
2024-11-09 21:34:17 +0000

    feat(next.config.static.ts): add GitHub Actions support for basePath and assetPrefix to handle deployment in GitHub environments

--- next.config.static.ts
+++ next.config.static.ts
@@ -9,3 +9,21 @@ export default {
     unoptimized: true
   }
 } as NextConfig
+const isGitHubActions = process.env.GITHUB_ACTIONS || false
+
+let basePath = ""
+if (isGitHubActions) {
+  const repo = (process.env.GITHUB_REPOSITORY, "/").split("/")[1]
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  reactStrictMode: true,
+  output: "export",
+  distDir: "dist",
+  basePath: basePath,
+  assetPrefix: basePath, // Ensure static assets use the same prefix
+  images: {
+    unoptimized: true
+  }
+}
